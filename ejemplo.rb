556e6a68f9898cec2f84798bfdf0d634201586f0
UrielxDAAL <L20290831@cdguzman.tecnm.mx>
2024-08-30 03:29:35 +0000

    actualizacion

--- ejemplo.rb
+++ ejemplo.rb
@@ -1,15 +1,17 @@
 class Padre
+    def initialize (nombre)
+        @nombre = nombre
+    end
     def say_hello
-        saludo = "Hola desde #{self}"
-        return saludo
+        "Hola #{@nombre} desde #{self}"
     end
 end
 
 class Hijo < Padre
 end
 
-saluda1=Padre.new
+saluda1=Padre.new("Darth")
 puts saluda1.say_hello
 
-saluda2= Hijo.new
+saluda2= Hijo.new("Anakin")
 puts saluda2.say_hello
